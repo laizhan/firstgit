1d184358e47953af1a6aad0ae46ab679d6bba4e8
laizhan <laizhan@live.com>
2018-08-08 16:09:39 +0800

    aspnetcore on jenkins.

---
+++ aspnetcoreonjenkins/aspnetcoreonjenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnetcoreonjenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
